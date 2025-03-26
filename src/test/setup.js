7e950363b10c2c5325df90f30b0d3192aa9d9e4a
Gayan Hewa <gayanhewa@gmail.com>
2025-03-27 00:26:13 +1100

    add the fixes to tests

--- src/test/setup.js
+++ src/test/setup.js
@@ -3,15 +3,9 @@ global.window = {};
 
 // Mock document object
 global.document = {
-<<<<<<< Updated upstream
   getElementById: () => null,
   querySelector: () => null,
   querySelectorAll: () => [],
-=======
-    getElementById: () => null,
-    querySelector: () => null,
-    querySelectorAll: () => []
->>>>>>> Stashed changes
 };
 
 // Mock Set constructor to handle the Set operations in the game
@@ -19,16 +13,8 @@ global.Set = Set;
 
 // Load the game code
 const gameCode = await Bun.file("../../public/game.js").text();
-<<<<<<< Updated upstream
 const script = new Function("window", gameCode);
 script(global.window);
 
 // Make the game function available globally for tests
 global.storyTypingGame = window.storyTypingGame;
-=======
-const script = new Function('window', gameCode);
-script(global.window);
-
-// Make the game function available globally for tests
-global.storyTypingGame = window.storyTypingGame; 
->>>>>>> Stashed changes
